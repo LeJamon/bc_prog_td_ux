195e696f76ca86fa0711c1a12334fb87612aa3d5
Hussenet Thomas <Lejamon.c@protonmail.com>
2022-12-05 17:28:43 +0100

    ajout affichage chainId

--- src/components/pages/Chain-info.js
+++ src/components/pages/Chain-info.js
@@ -3,17 +3,17 @@ import { ethers } from "ethers";
 import Connect from "../../utils/Connect";
 import Web3 from "web3";
 function ChainInfo() {
-  Connect(); 
   const {ethereum} = window; 
-  window.web3 = new Web3(window.ethereum);
-  const account = ethereum.selectedAddress;  
+  //window.web3 = new Web3(window.ethereum);
+  const account = ethereum.selectedAddress; 
+  const chainId =  ethereum.networkVersion;  
   
     return (
         <div>
             <div>{account}</div>
             <br></br>
+            <div>{chainId}</div>
             <br></br>
-            
         </div>
     )
 }
