e9f28c508d6d031546273f68f30283d4e31bb1f4
Hussenet Thomas <Lejamon.c@protonmail.com>
2022-12-08 18:58:26 +0100

    ajout du bouton de mint

--- src/pages/FakeBayc.js
+++ src/pages/FakeBayc.js
@@ -4,23 +4,36 @@ import Web3 from 'web3';
 
 function FakeBayc(){
 
+// state zone 
 const [supply, Setsupply] = useState(null); 
 const [name, Setname] = useState(null); 
+
+//contract zone 
 const contract_abi = require("../contract/FakeBayc.json").abi; 
 const contract_address = "0x1dA89342716B14602664626CD3482b47D5C2005E"; 
 
+// declare the contract
+let web3 = new Web3(window.ethereum);
+var contract = new web3.eth.Contract(contract_abi, contract_address);
 
  
+
 
 async function GetSupplyAndName(){
  
-    let web3 = new Web3(window.ethereum);
-    var contract = new web3.eth.Contract(contract_abi, contract_address);
     let a = await contract.methods.tokenCounter().call();
     Setsupply(a); 
     let b = await contract.methods.name().call();
     Setname(b); 
 }
+
+async function MintNft(){
+ 
+    //use window.ethereum to get the account
+    const accounts = await window.ethereum.request({method: 'eth_requestAccounts' }); 
+    await contract.methods.claimAToken().send({from: accounts[0]}).then(console.log); 
+}
+
 
  
     return(
@@ -28,6 +41,7 @@ async function GetSupplyAndName(){
         <button onClick={GetSupplyAndName}>get Name and Supply</button>
         <div>{name}</div>
         <div>{supply}</div>
+        <button onClick={MintNft}>claim a free nft</button>
         </div>
     )
 }
