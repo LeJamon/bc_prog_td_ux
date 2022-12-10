1cef166e0dab75fc6e4bc65f0fb8f97e85d1fe5c
Hussenet Thomas <Lejamon.c@protonmail.com>
2022-12-10 23:42:22 +0100

    Add the meebit mint button Still to do : alert message if the network change

--- src/pages/FakeMeebits.js
+++ src/pages/FakeMeebits.js
@@ -1,5 +1,42 @@
+import Web3 from "web3";
+import { useState } from "react";
+
 export default function FakeMeebits(){
-    return(
-        <div>bite</div>
-    )
+    
+  const jsonSignature = require("../contract/sig.json"); 
+
+  const contract_abi = require("../contract/Claimer.json").abi; 
+  const contract_address = "0x5341e225Ab4D29B838a813E380c28b0eFD6FBa55"; 
+  
+  let web3 = new Web3(window.ethereum);
+
+  var contract = new web3.eth.Contract(contract_abi, contract_address);
+
+  const[tokenId, setTokenId] = useState();
+
+
+  async function Mint(){
+    const accounts = await window.ethereum.request({method: 'eth_requestAccounts' });
+    // check that we can mint the token
+    if(await contract.methods.tokensThatWereClaimed(tokenId).call()){
+    alert("This token has alredy been mint"); 
+    throw Error("already minted");  
+  }else{
+    const _signature = jsonSignature[tokenId].signature;  
+    await contract.methods.claimAToken(tokenId,_signature).send({from: accounts[0]});
+    alert("token has been minted");
+  }
+}
+
+
+  const handleChamp = (event)=>{
+     setTokenId(event.target.value)
+  }
+
+return(
+    <div>
+      <input type="text"value={tokenId} onChange={e=>handleChamp(e)}/>
+      <button onClick={Mint}> mint</button>
+    </div>
+)
 }
